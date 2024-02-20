afc20af8a18f4757a4c7db56d0a4d53c51cc0873
Jared Murphy <jlmurphy.sa@gmail.com>
2024-02-20 09:43:11 +0200

    add docker-compose file with mongo, implement query resolver logic, define TrackAPI

--- src/dataSources/TrackAPI.ts
+++ src/dataSources/TrackAPI.ts
@@ -2,4 +2,28 @@ export class TrackAPI {
   constructor() {
 
   }
+
+  async findTrack(name, artist) {
+
+  }
+
+  async getAllTracks() {
+
+  }
+
+  async getTrack(id) {
+
+  }
+
+  async updateTrack(id, data) {
+
+  }
+
+  async deleteTrack(id) {
+
+  }
+  
+  async createTrack(data) {
+    
+  }
 }
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -9,20 +9,31 @@ export default {
     { name, artistName }: { name: string, artistName: string },
     { dataSources }: AppContext
   ): Promise<ITrack> {
-    return {}
+    let track = await dataSources.trackAPI.findTrack(name, artistName)
+
+    if (!track) {
+      const remoteTrack = await dataSources.externalAPI.fetchTrack(name, artistName)
+      if (!remoteTrack) {
+        return null
+      }
+
+      track = await dataSources.trackAPI.createTrack(remoteTrack)
+    }
+    
+    return track
   },
   async getAllTracks(
     _: undefined,
     __: undefined,
     { dataSources }: AppContext
   ): Promise<Array<ITrack>> {
-    return []
+    return dataSources.trackAPI.getAllTracks()
   },
   async getTrackById(
     _: undefined,
     { internalId }: { internalId: string },
     { dataSources }: AppContext
   ): Promise<ITrack> {
-    return {}
+    return dataSources.trackAPI.getTrack(internalId)
   }
 }
